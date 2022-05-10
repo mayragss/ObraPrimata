786f1f496a4467e73d3469d7dda14029840a0710
May Gomes <35641653+mayragss@users.noreply.github.com>
2022-05-10 20:13:58 -0300

    fix branch

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 const route= require('express').Router();
 
-
-route.get('/old', (req,res)=>{
-    res.render('index')
-});
-
 route.get('/', (req,res)=>{
-    res.render('index-safari')
+    res.render('index')
 });
 
 
