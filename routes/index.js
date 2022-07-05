4d525123301470a54a7cfa4118bbc8c7fb552017
May Gomes <35641653+mayragss@users.noreply.github.com>
2022-07-05 20:26:54 -0300

    add portfolio and change instinct size

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ route.get('/', (req,res)=>{
     res.render('index')
 });
 
+route.get('/portfolio', (req,res)=>{
+    res.download('Obra Primata.pdf')
+});
+
+route.get('*', (req,res)=>{
+    res.render('index')
+});
 
 module.exports = route;
